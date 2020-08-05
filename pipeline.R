386602d893a77f11b22f0eaa4b29fffe693b3a68
nfj1380 <nfountainjones@gmail.com>
2020-08-05 12:01:27 +1000

    Updated get graph feature function as well as pipeline function

---
+++ pipeline.R
@@ -0,0 +1,21 @@
+#' Vignette to run the EpicGraph package
+
+#Load packages - keep this to a minimum
+library(igraph)
+library(EpiModel)
+library(intergraph)
+library(DiagrammeR)
+library(tidyverse)
+
+# load all function codes. This will disappear when we formally make this a function
+source(".getGraphFeatures.R")
+
+
+#-----------------------------------------------------------------------------------
+#Step 1: Sumamrise network features
+#-----------------------------------------------------------------------------------
+folder_name = c('Networks') #.edges file need to be in the working directory too at this stage.
+
+NetSum <- getGraphFeatures(folder_name)
+
+NetSum
